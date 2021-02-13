e9fcaa57b5cd539f51d3c5595a3dc50a35337edd
mantinha <adriandesigner@hotmail.com>
2021-02-13 13:04:53 -0300

    update project & add BeerDTO

---
+++ src/main/java/com/example/mybrewery/MyBreweryApplication.java
@@ -0,0 +1,13 @@
+package com.example.mybrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBreweryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mybrewery/MyBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mybrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
