0c85064994fa35cd71ac4863778758eca3522185
mantinha <adriandesigner@hotmail.com>
2021-02-16 13:45:33 -0300

    add hibernate & update BeerDTOV2

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Aplicação de uma Cervejaria
 ![](https://img.shields.io/badge/dependency-Web-brightgreen)
 ![](https://img.shields.io/badge/dependency-Actuator-brightgreen)
 ![](https://img.shields.io/badge/dependency-JPA-brightgreen)
+![](https://img.shields.io/badge/dependency-Hibernate-brightgreen)
--- src/main/java/com/example/mybrewery/web/model/v2/BeerDTOV2.java
+++ src/main/java/com/example/mybrewery/web/model/v2/BeerDTOV2.java
@@ -1,8 +1,16 @@
 package com.example.mybrewery.web.model.v2;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,15 +21,31 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class BeerDTOV2 {	
-	private BigDecimal price;
-	private String beerName;
-	private BeerStyleEnum beerStyle;
-	private Integer quantityOnHand;
-	private Integer version;
-	private Long upc;	
-	private OffsetDateTime createdDate;
-	private OffsetDateTime lastModifiedDate;
-	private UUID id;
+public class BeerDTOV2 implements Serializable {
+	private static final long serialVersionUID = 5815566940065181210L;
 	
+	@Null
+	private UUID id;
+	@Null
+	private Integer version;
+	
+	@Null
+	@JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
+	private OffsetDateTime createdDate;
+	@Null
+	@JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ssZ", shape=JsonFormat.Shape.STRING)
+	private OffsetDateTime lastModifiedDate;
+	
+	@NotBlank
+	private String beerName;
+	@NotNull
+	private BeerStyleEnum beerStyle;
+	@NotNull
+	private Long upc;
+	
+	@JsonFormat(shape=JsonFormat.Shape.STRING)
+	@Positive
+	@NotNull
+	private BigDecimal price;
+	private Integer quantityOnHand;	
 }
